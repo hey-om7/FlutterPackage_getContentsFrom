7602e3056e07815eb86972785d674283e61b9486
Om Ambarkar <heyom7@gmail.com>
2021-05-12 08:47:58 +0530

    First Commit

---
+++ lib/get_file_contents_from.dart
@@ -0,0 +1,56 @@
+library get_file_contents_from;
+
+import 'dart:io';
+import 'dart:io' as io;
+
+class FileManager {
+  String pathDirectory = '/storage/emulated/0/';
+  Future<List> getContentsFromPath(String path) async {
+    List file = [];
+
+    var _fullPaths = [];
+    pathDirectory = path;
+    file = io.Directory(path).listSync();
+
+    for (int i = 0; i < file.length; i++) {
+      _fullPaths.add(file[i].path.toString());
+    }
+    return _fullPaths;
+  }
+
+  Future<List> getContentsFilenamesFromPath(String path) async {
+    List file = [];
+    var _fileNames = [];
+    pathDirectory = path;
+    file = io.Directory(path).listSync();
+
+    for (int i = 0; i < file.length; i++) {
+      _fileNames.add((file[i].path.toString().split('/').last));
+    }
+    return _fileNames;
+  }
+
+  Future<String> goBack() async {
+    String _tempDirectory = pathDirectory;
+    if (_tempDirectory != "/storage/emulated/0/") {
+      int _length = _tempDirectory
+          .split("/")[_tempDirectory.split("/").length - 2]
+          .length;
+      String _finalPath =
+          _tempDirectory.substring(0, _tempDirectory.length - _length - 1);
+      print(_finalPath);
+      return _finalPath;
+    } else {
+      String _finalPath = "/storage/emulated/0/";
+      return _finalPath;
+    }
+  }
+
+  Future<String> getCurrentDirectory() async {
+    if (Directory.current.path == "/") {
+      return '/storage/emulated/0/';
+    } else {
+      return '/storage/emulated/0/' + Directory.current.path;
+    }
+  }
+}
